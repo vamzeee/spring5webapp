195507c70882b7d38298960f2df9549d30f553e3
Vamsee Meduri <vgopalmeduri@gmail.com>
2021-01-11 11:24:52 +0530

    First Commit

--- src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
+++ src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Spring5webappApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Spring5webappApplication.class, args);
 	}
